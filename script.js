ac94234f6b2932acfc4466215272760276ac4114
INWYTIA <inwytia@gmail.com>
2019-12-13 20:20:46 -0600

    Fixed getCharType and added getChar method

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 var password = {
     length : 0,
+    charTypes : ['alpha', 'num', 'special'],
     characters : {
         alpha : ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
         num : ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
         special : [' ', '!', '\\', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'],
     },
     getCharType : function() {
-        return password[characters][Math.floor(Math.random() * password.characters.length)]
+        return 'this.characters.' + this.charTypes[Math.floor(Math.random() * this.charTypes.length)]
+
+    },
+    getChar : function(type) {
+        return type[Math.floor(Math.random() * type.length)]
     }
 }
 // Computes random numbers according to array length
-// choices[Math.floor(Math.random() * choices.length)]
+// ]
+console.log (password.getCharType())
+console.log (password.getChar(password.getCharType()))
