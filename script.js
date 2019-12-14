9478eddd0f6eafa1cf09be12e8fe1db79892e6d5
INWYTIA <inwytia@gmail.com>
2019-12-13 21:02:49 -0600

    Updated getChar to fetch a random character of a DESIRED type

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ var password = {
     length : 0,
     targetLength : 0,
     wantUpper : confirm('Uppercase?'),
+    upperChars : 0,
     wantLower : confirm('Lowercase?'),
+    lowerChars : 0,
     wantNum : confirm('Numbers?'),
+    numChars : 0,
     wantSpecial : confirm('Special?'),
+    specialChars : 0,
     charTypes : ['alpha', 'num', 'special'],
     characters : {
         alpha : ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
@@ -12,14 +16,28 @@ var password = {
         special : [' ', '!', '\\', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'],
     },
     getCharType : function() {
-        return 'this.characters.' + this.charTypes[Math.floor(Math.random() * this.charTypes.length)]
+        return this.charTypes[Math.floor(Math.random() * this.charTypes.length)]
 
     },
     getChar : function(type) {
-        return type[Math.floor(Math.random() * type.length)]
-    }
+        if (type === 'alpha') {
+            if (this.wantUpper) {
+                return this.characters.alpha[Math.floor(Math.random() * this.characters.alpha.length)].toUpperCase
+            } else if (this.wantLower) {
+                return this.characters.alpha[Math.floor(Math.random() * this.characters.alpha.length)]
+            }
+        } else if (type === 'num') {
+            if (this.wantNum) {
+                return this.characters.num[Math.floor(Math.random() * this.characters.num.length)]
+            }
+        } else if (type === 'special') {
+            if (this.wantSpecial) {
+                return this.characters.special[Math.floor(Math.random() * this.characters.special.length)]
+            }
+        }
+    },
 }
 // Computes random numbers according to array length
-// ]
-console.log (password.getCharType())
+// this.characters.alpha[Math.floor(Math.random() * this.characters.alpha.length)]
+console.log (password.getChar(this.getCharType()))
 console.log (password.wantUpper)
